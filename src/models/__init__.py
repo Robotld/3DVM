739afd586357c60ade48e1efbc570a06a966ecef
Robotld <1147675514@qq.com>
2025-03-31 15:34:21 +0800

    Rename ViT_3D to prodiViT_3D for improved clarity.
    Updated the module and its references to ensure consistency and better reflect its purpose. This change helps differentiate it from other potential variants of ViT models.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,7 @@ from .lr_scheduler import WarmupScheduler
 # 导入自定义模块
 from .transform import create_transforms
 from .CrossVakudator import CrossValidator
-from .ViT_3D import ViT3D
+from .prodiViT_3D import ViT3D
 from .dataset import NoduleDataset, RecurrenceDataset
 from .losses import Enhanced3DVITLoss, BoundaryFlowLoss, EdgeAwareFlowModule, FocalLoss, build_loss
 from .balanced_sampler import BalancedBatchSampler
